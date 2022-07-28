0ea0c7a619f9515fa06ed0cbb4f5808bfecd80bf
PLYER LOUIS <louis.plyer@unistra.fr>
2022-07-28 13:52:49 +0200

    Version updated

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2022072601;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022072800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020060900;        // Requires this Moodle version.
 $plugin->component = 'atto_molstructure';  // Full name of the plugin (used for diagnostics).
 
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->release = 1.2;
+$plugin->release = 1.3;
